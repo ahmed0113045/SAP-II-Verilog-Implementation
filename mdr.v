bbb765ef43ff37bbb5fc9c102719ed0a0ebca51f
ahmed0113045 <31720715+ahmed0113045@users.noreply.github.com>
2020-02-29 17:55:53 +0200

    Update mdr.v

--- mdr.v
+++ mdr.v
@@ -1,28 +1,30 @@
 /**
-* Module: Memory Data Register (MDR)
+* Module: MEwory Data Register (MDR)
 * haven't complete test bench yet
 *
-* Brief : The memory data register (MDR) is an 8-bit buffer register , Its output sets up the RAM. 
-* The memory data register receives data from the bus before a write operation, and it sends data to the bus after a read operation. 
+* Brief : The mEwory data register (MDR) is an 8-bit buffer register , Its output sets up the RAM. 
+* The mEwory data register receives data from the bus before a write operation, and it sends data to the bus after a read operation. 
 *
 * Input :
 * WBUS = Data from WBUS.
-* data = data from Memory
+* data = data from MEwory
 * nLw  = Enable MDR to Load WBUS data 	(0 = enable)
-* nLr  = Enable MDR to Load Memory data (0 = enable)
-* Em   = Enable MDR to write on WBUS
+* nLr  = Enable MDR to Load MEwory data (0 = enable)
+* Ew   = Enable MDR to write on WBUS
+* Er   = Enable MDR to write on Memory
 * CLK  = Clock
 *
 * Output :
 * WBUS = data to WBUS
-* data = data to Memory
+* data = data to MEwory
 */
 module mdr (
 			inout [7:0] WBUS,
 			inout [7:0] data,
 			input       nLw ,
 			input		nLr,
-			input		Em,
+			input		Ew,
+			input		Er,
 			input       CLK );		
 	
 	reg [7:0] mdrreg ;
@@ -30,8 +32,8 @@ module mdr (
 	parameter Zero_State     = 8'b0000_0000;
 	parameter High_Impedance = 8'bzzzz_zzzz;
 	
-	assign WBUS = Em? mdrreg : High_Impedance;
-	assign data = mdrreg;
+	assign WBUS = Ew? mdrreg : High_Impedance;
+	assign data = Er? mdrreg : High_Impedance;
 	
     initial begin	mdrreg <= Zero_State;	end
     
@@ -50,7 +52,8 @@ module t_mdr;
 	reg         CLK;
 	reg         nLw;
 	reg			nLr;
-	reg			Em;
+	reg			Ew;
+	reg			Er;
 	
 	reg [7:0] temp_WBUS;
 	reg [7:0] temp_data;
@@ -60,17 +63,17 @@ module t_mdr;
 	
 	parameter Zero_State     = 8'b0000_0000;
 	parameter High_Impedance = 8'bzzzz_zzzz;
-	mdr MDR (WBUS,data,nLw,nLr,Em,CLK);	
+	mdr MDR (WBUS,data,nLw,nLr,Ew,Er,CLK);	
 	initial begin 
 		CLK = 0 ;
 		forever #50 CLK = ~CLK ;
 	end
 
 	initial begin 
-			nLw = 1;	nLr = 1;	Em = 0;		temp_WBUS = 8'h15;	temp_data = 8'h17;	//do nothing	
-	#100	nLw = 0;	nLr = 1;	Em = 0;		temp_WBUS = 8'h25;	temp_data = 8'h27;	//load from WBUS
-	#100	nLw = 1;	nLr = 0;	Em = 0;		temp_WBUS = 8'h35;	temp_data = 8'h37;	//load form Memory
-	#100	nLw = 1;	nLr = 1;	Em = 1;		temp_WBUS = 8'h45;	temp_data = 8'h47;	//write on WBUS
+			nLw = 1;	nLr = 1;	Ew = 0;	Er = 0;		temp_WBUS = 8'h15;	temp_data = 8'h17;	//do nothing	
+	#100	nLw = 0;	nLr = 1;	Ew = 0;	Er = 0;		temp_WBUS = 8'h25;	temp_data = 8'h27;	//load from WBUS
+	#100	nLw = 1;	nLr = 0;	Ew = 0;	Er = 1;		temp_WBUS = 8'h35;	temp_data = 8'h37;	//load form MEwory
+	#100	nLw = 1;	nLr = 1;	Ew = 1;	Er = 0;		temp_WBUS = 8'h45;	temp_data = 8'h47;	//write on WBUS
 
 	
 	end
