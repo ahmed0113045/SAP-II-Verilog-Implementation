e7f59342fc3fe43a74ec6d95f945275b42226451
HeshamRmah <49096517+HeshamRmah@users.noreply.github.com>
2020-02-29 18:09:03 +0200

    Add files via upload

--- instruction_register.v
+++ instruction_register.v
@@ -1,64 +1,95 @@
 /**
-* Module: Instruction Registor (IR)
-*
-* Brief : Because SAP-2 has more instructions than SAP-1, we will use 8 bits for the op code rather than 4. 
-* An 8-bit op code can accommodate 256 instructions. SAP-2 has only 42 instructions, so there will be no problem coding them with 8 bits.
-* Using an 8-bit op code also allows upward compat-ibility with the 8080/8085 instruction set because it is based on an 8-bit op code. 
-* As mentioned earlier, all SAP instructions are identical with 8080/8085 instructions. 
+* Instruction Registor (IR)
 *
 * Input :
-* WBUS   = Data from WBUS
 * CLK    = Clock
 * nLi    = Load Load from WBUS (8-bit). 0 = load
 * nEi    = Output Address. 0 = Output to lower part of WBUS (4-bit)
 * nCLR   = Clear
+* WBUS   = Data from WBUS
 *
 * Output :
+* Bits [3 : 0] => address
+* addr   = Address to WBUS
+* Bits [7 : 4] => opcode
 * opcode = Op Code to Determine the Operation
 */
 module instruction_register (
-							output [7:0] opcode,
-							input  [7:0] WBUS,
+							output [3:0] addr,
+							output [3:0] opcode,
 							input        CLK,
 							input        nLi,
 							input        nEi,
-							input        nCLR );
+							input        nCLR,
+							input  [7:0] WBUS );
 
-	parameter Zero_State     = 8'b0000_0000;
+	parameter High_Impedance = 4'bzzzz;
+	parameter Zero_State     = 4'b0000;
 
-    reg [7:0] opcode ;
+    reg [3:0] op_reg ;
+    reg [3:0] add_reg ;
 
     initial begin
-		opcode <= Zero_State;
+		op_reg    <= Zero_State;
+		add_reg   <= High_Impedance;
     end
     
-	// Write your Code here :
-	
-	
-	
-	
-	
+	assign addr   = (!nEi) ? add_reg : High_Impedance;
+    assign opcode = op_reg ;
+    
+    always @(posedge CLK) begin
+
+        if(!nCLR) begin
+            op_reg    <= Zero_State;
+            add_reg   <= High_Impedance;
+        end
+
+        else begin
+
+			if(!nLi) begin
+				// Load the data
+                op_reg   <= WBUS[7:4];
+	            add_reg  <= WBUS[3:0];
+            end
+
+            else begin
+                // DO Nothing
+                op_reg   <= op_reg;
+                add_reg  <= add_reg;
+            end
+        end
+    end
 endmodule
-/*************************************** Test Bench ***************************************/
+/***************************************************************************/
 module t_instruction_register ;
 
-	wire [7:0] opcode;
-	reg  [7:0] WBUS;
-	reg        CLK;
-	reg        nLi,nEi,nCLR ;
+wire [3:0] addr;
+wire [3:0] opcode;
+reg  [7:0] WBUS;
+reg        CLK;
+reg        nLi,nEi,nCLR ;
 
-	instruction_register IR (opcode,WBUS,CLK,nLi,nEi,nCLR);
+instruction_register IS (addr,opcode,CLK,nLi,nEi,nCLR,WBUS );
 
-	initial begin 
-		CLK = 0 ;
-		forever #50 CLK = ~CLK ;
-	end
+initial begin 
+CLK = 0 ;
+forever #50 CLK = ~CLK ;
+end
 
-	initial begin 
-		
-		// Write your Test Cases here :
-		
-		
-	end
+initial begin 
+
+     nCLR = 0 ; nLi = 1 ; nEi = 1 ; WBUS = 8'hca;
+#100 nCLR = 0 ; nLi = 1 ; nEi = 1 ; WBUS = 8'hca;
+#100 nCLR = 1 ; nLi = 0 ; nEi = 1 ; WBUS = 8'hca;
+#100 nCLR = 1 ; nLi = 0 ; nEi = 0 ; WBUS = 8'hca;
+#100 nCLR = 1 ; nLi = 1 ; nEi = 1 ; WBUS = 8'hfe;
+#100 nCLR = 1 ; nLi = 1 ; nEi = 0 ; WBUS = 8'hfe;
+#100 nCLR = 1 ; nLi = 0 ; nEi = 1 ; WBUS = 8'hfe;
+#100 nCLR = 1 ; nLi = 0 ; nEi = 0 ; WBUS = 8'h99;
+#100 nCLR = 1 ; nLi = 0 ; nEi = 0 ; WBUS = 8'h8e;
+#100 nCLR = 1 ; nLi = 1 ; nEi = 0 ; WBUS = 8'h57;
+#100 nCLR = 1 ; nLi = 0 ; nEi = 0 ; WBUS = 8'h67;
+
+end
 
 endmodule
