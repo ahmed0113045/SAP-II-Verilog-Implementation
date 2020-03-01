85682f88a63c8aafc748957d5e187ad0f528b0fe
ahmed0113045 <31720715+ahmed0113045@users.noreply.github.com>
2020-03-01 09:39:52 +0200

    Update out_port_4.v

--- out_port_4.v
+++ out_port_4.v
@@ -8,6 +8,8 @@
 *
 * Input :
 * WBUS = Data from WBUS
+* Lo4  = load from WBUS
+* shift_r = shift register right to transfer data serially
 *
 * Output :
 * serial_out = Data out in serial form
@@ -16,24 +18,46 @@
 module out_port_4 (
 					output serial_out,
 					output acknowedge,
-					input [7:0] WBUS );
-
-	// Write your Code here :
+					input [7:0] WBUS, 
+					input CLK,
+					input Lo4,
+					input shift_r);
+					
+	reg [7:0] register4;
 	
+	assign serial_out = register4[0];
+	assign acknowedge = register4[7];
 	
-	
-	
+	always @(posedge CLK) begin
+		if(Lo4)			register4 <= WBUS;
+		else if(shift_r)	register4 <= (register4>>1);
+		else 			register4 <= register4;
+	end
 	
 endmodule
 /*************************************** Test Bench ***************************************/
 module t_out_port_4 ;
 	
-	wire serial_out,acknowedge;
-	reg [7:0] WBUS ;
+	wire serial_out, acknowedge;
+	reg [7:0] WBUS; 
+	reg CLK, Lo4, shift_r;
+	
+	out_port_4 out(serial_out,acknowedge,WBUS,CLK,Lo4,shift_r);
 	
 	initial begin 
-		
-		// Write your Test Cases here :
+		CLK = 0 ;
+		forever #50 CLK = ~CLK ;
+	end
+	
+	initial begin 
+			Lo4 = 0;	shift_r = 0;	WBUS = 8'h15;
+	#100	Lo4 = 1;	shift_r = 0;	WBUS = 8'h25;
+	#100	Lo4 = 1;	shift_r = 1;	WBUS = 8'h35;
+	#100	Lo4 = 0;	shift_r = 1;	WBUS = 8'h45;
+	#100	Lo4 = 0;	shift_r = 0;	WBUS = 8'h55;
+	#100	Lo4 = 0;	shift_r = 0;	WBUS = 8'h65;
+	#100	Lo4 = 0;	shift_r = 0;	WBUS = 8'h75;
+	#100	Lo4 = 1;	shift_r = 0;	WBUS = 8'h85;
 		
 		
 		
