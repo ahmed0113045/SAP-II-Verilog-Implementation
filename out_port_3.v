aaca0eb5a9d75db4af8bb988abb3619d01ee4e58
ahmed0113045 <31720715+ahmed0113045@users.noreply.github.com>
2020-03-01 09:13:22 +0200

    Update out_port_3.v

--- out_port_3.v
+++ out_port_3.v
@@ -6,17 +6,23 @@
 *
 * Input :
 * WBUS   = Data from WBUS 
+* Lo3	 = load data from bus
+* CLK    = clock signal
 *
 * Output :
 * out    = Data to Hexadecimal Display
 */
 module out_port_3 (
-					output [7:0] out,
-					input  [7:0] WBUS );
+					output reg [7:0] out,
+					input  [7:0] WBUS,
+					input  CLK,
+					input Lo3 );
 
-	// Write your Code here :
+	always @(posedge CLK) begin
+		if(Lo3)	out <= WBUS;
+		else	out <= out;
 	
-	
+	end
 	
 endmodule
 /*************************************** Test Bench ***************************************/
@@ -24,10 +30,18 @@ module t_out_port_3 ;
 
 	wire [7:0] out;
 	reg [7:0] WBUS ;
-
+	reg CLK;
+	reg Lo3;
+	out_port_3 out_3(out,WBUS,CLK,Lo3 );
 	initial begin 
-		
-		// Write your Test Cases here :
+		CLK = 0 ;
+		forever #50 CLK = ~CLK ;
+	end
+	initial begin 
+			Lo3 = 0;	WBUS = 8'h15;
+	#100	Lo3 = 1;	WBUS = 8'h25;
+	#100	Lo3 = 1;	WBUS = 8'h35;
+	#100	Lo3 = 1;	WBUS = 8'h45;
 		
 		
 	end
